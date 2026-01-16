3beaeb9dc6d8dcece3cc3833d6ad40184192f39b
JyunWaan <44659389+JyunWaan@users.noreply.github.com>
2026-01-16 08:21:21 +0000

    Deploying to gh-pages from @ JyunWaan/nav@73184d7047ce9cb1bc4fd7d811f3d9b130b7dd44 🚀

---
+++ chunk-FUT5YGOV.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-WRYBMGNN.js";import{Ab as e,sb as r}from"./chunk-K6RJ5JVQ.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
